53ef877eee45e2bc571463419f59dad29be47904
Sumit Beniwal <99647169+SumitB1412@users.noreply.github.com>
2022-05-07 15:59:24 +0530

    pushed

---
+++ src/global-styles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family:Arial, Helvetica, sans-serif   ;
+    position: relative;
+    /* top: 300px; */
+    background: white;
+}`;
