290867fd04b909dff48a8de32256064e12d6fc94
Sumit Beniwal <99647169+SumitB1412@users.noreply.github.com>
2022-05-09 20:27:24 +0530

    Create Checkout.jsx

---
+++ src/CheckOut/Checkout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Shipping_address } from "./Shipping_address";
+import { Payment } from "./Payment";
+import { OrderRecipt } from "./OrderRecipt";
+const Checkout = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <Shipping_address />
+        <Payment />
+      </div>
+      <div>
+        <OrderRecipt />
+      </div>
+    </div>
+  );
+};
+
+export { Checkout };
